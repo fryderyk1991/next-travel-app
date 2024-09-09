f19720d258deab255351eb1f3d3ab13befd53046
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2024-09-09 20:58:05 +0200

    added packages - three && fiber

--- app/ui/Hero.tsx
+++ app/ui/Hero.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-
+// import { LockClosedIcon } from '@heroicons/react/16/solid';
 const Hero = () => {
   return (
-    <div className='shadow-lg shadow-primary mx-auto w-[90%] rounded-lg'>
-        <div className='text-font-primary p-4'>
-          <h1 className='text-7xl'>Travel</h1>
-          <h2 className='text-3xl mt-5'>Unlock the World</h2>
-          <p className='mt-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, facilis?</p>
+      <div className="mx-auto w-[90%] p-4">
+        <div className="text-font-primary">
+          <h1 className="text-6xl mt-5">Travel</h1>
+          <h2 className="text-3xl mt-6">Unlock the World</h2>
+          <p className="mt-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Asperiores, facilis?
+          </p>
         </div>
-    </div>
-  )
+      </div>
+  );
 }
 
 export default Hero 
